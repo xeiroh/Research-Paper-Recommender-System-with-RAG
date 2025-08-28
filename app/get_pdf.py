b0bc243f496bd6e4f4213884a788a6a23ecd3ee1
Gokul Ganesan <gokulganesan3@gmail.com>
2025-08-28 19:29:08 -0400

    del pdfs

--- app/get_pdf.py
+++ app/get_pdf.py
@@ -7,24 +7,29 @@ import io
 
 DOWNLOAD_PATH = os.path.join(settings.root, "downloads")
 if not os.path.exists(DOWNLOAD_PATH):
-    os.makedirs(DOWNLOAD_PATH)
+	os.makedirs(DOWNLOAD_PATH)
 
 
 def is_valid_pdf(data: bytes) -> bool:
-    try:
-        PdfReader(io.BytesIO(data))
-        return True
-    except Exception:
-        return False
+	try:
+		PdfReader(io.BytesIO(data))
+		return True
+	except Exception:
+		return False
 
 
 def get_pdf(url: str) -> bytes:
-    file_name = url.split("/")[-1]
-    file_path = os.path.join(DOWNLOAD_PATH, file_name)
-    response = requests.get(url)
-    with open(file_path, "wb") as f:
-        f.write(response.content)
-    if not is_valid_pdf(response.content):
-        os.remove(file_path)
-        return None
-    return file_path
+	# delete all existing pdfs in DOWNLOAD_PATH
+	for file in os.listdir(DOWNLOAD_PATH):
+		os.remove(os.path.join(DOWNLOAD_PATH, file))
+	
+	# get pdfs
+	file_name = url.split("/")[-1]
+	file_path = os.path.join(DOWNLOAD_PATH, file_name)
+	response = requests.get(url)
+	with open(file_path, "wb") as f:
+		f.write(response.content)
+	if not is_valid_pdf(response.content):
+		os.remove(file_path)
+		return None
+	return file_path
